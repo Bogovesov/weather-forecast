0a32ff8bab85ec3c0c8a7f81b647e89ed1d71202
sergey <bogovesov@hotmail.com>
2018-06-01 08:26:04 +0300

    init

---
+++ src/com/alevel/j4/Temperature.java
@@ -0,0 +1,16 @@
+package com.alevel.j4;
+
+public class Temperature {
+    public static boolean isValid(String temperature) {
+        try {
+            double value = Double.parseDouble(temperature);
+            if (value < -273.15) {
+                return false;
+            }
+        } catch (NumberFormatException e) {
+            System.out.println(temperature);
+            return false;
+        }
+        return true;
+    }
+}
---
+++ src/com/alevel/j4/TemperatureFormatException.java
@@ -0,0 +1,7 @@
+package com.alevel.j4;
+
+public class TemperatureFormatException extends Exception {
+    //
+    //
+    //
+}
